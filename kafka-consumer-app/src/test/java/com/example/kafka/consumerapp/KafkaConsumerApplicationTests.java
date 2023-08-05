474fece66ba8f433352f89a5bf39d4c36fcd184a
trico <yafet.kubrom@gmail.com>
2023-08-05 13:41:41 -0500

    created appropriate modules (producer,consumer,testing)

---
+++ kafka-consumer-app/src/test/java/com/example/kafka/consumerapp/KafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka.consumerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ kafka-functional-test/src/main/java/com/example/kafka/kafkafunctionaltest/KafkaFunctionalTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka.kafkafunctionaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaFunctionalTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaFunctionalTestApplication.class, args);
+    }
+
+}
